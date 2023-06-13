f1c8ab1f0a979ba4c947f404ca2e4fea5bc121db
Scott A. Idler <scott.a.idler@gmail.com>
2023-06-13 14:10:19 -0700

    bounced to latest clap ver; added eyre; made more robust using regex, lowercased

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use clap::Parser;
+use eyre::Result;
+use regex::Regex;
 use std::fs;
 use std::path::Path;
-use clap::Parser;
 
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[derive(Parser)]
+#[command(
+    name = "dashify",
+    about = "lowercases, and removes spaces and other chars in file names"
+)]
+#[command(version = "0.1.0")]
+#[command(author = "Scott A. Idler <scott.a.idler@gmail.com>")]
+#[command(arg_required_else_help = true)]
 struct Args {
     #[arg(short, long)]
     recursive: bool,
@@ -12,38 +20,42 @@ struct Args {
     paths: Vec<String>,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
     for path in &args.paths {
         // Check if the provided path is a file or a directory
         if Path::new(path).is_file() {
-            rename_file(path);
+            rename_file(path)?;
         } else if Path::new(path).is_dir() {
-            rename_files_in_dir(path, args.recursive);
+            rename_files_in_dir(path, args.recursive)?;
         } else {
             eprintln!("Error: {} is not a file or directory", path);
             std::process::exit(1);
         }
     }
+    Ok(())
 }
 
-fn rename_file(path: &str) {
-    // Rename the file if it has a space in its name
-    if path.contains(" ") {
-        let new_path = path.replace(" ", "-");
-        fs::rename(path, new_path).expect("Error renaming file");
+fn rename_file(path: &str) -> Result<()> {
+    let re = Regex::new(r"[, -]+")?;
+    if re.is_match(path) {
+        let new_path = re.replace_all(path, "-");
+        let new_path = new_path.to_lowercase();
+        fs::rename(path, new_path.as_ref() as &std::path::Path)?;
     }
+    Ok(())
 }
 
-fn rename_files_in_dir(dir: &str, recursive: bool) {
+fn rename_files_in_dir(dir: &str, recursive: bool) -> Result<()> {
     // Iterate through the directory and rename files with spaces in their names
-    for entry in fs::read_dir(dir).expect("Error reading directory") {
-        let entry = entry.expect("Error reading directory entry");
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
         let path = entry.path();
         if path.is_file() {
-            rename_file(&path.to_string_lossy());
+            rename_file(&path.to_string_lossy())?;
         } else if recursive && path.is_dir() {
-            rename_files_in_dir(&path.to_string_lossy(), true);
+            rename_files_in_dir(&path.to_string_lossy(), true)?;
         }
     }
+    Ok(())
 }
